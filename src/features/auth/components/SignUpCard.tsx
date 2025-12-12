acaebfb979a9620708ab83d218223055cdd1d09a
Syed Laeeq Ahmed <laeeqe@icloud.com>
2025-12-12 20:09:09 +0500

    Updated SignIn/SignUp UI

---
+++ src/features/auth/components/SignUpCard.tsx
@@ -0,0 +1,98 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Separator } from '@radix-ui/react-separator'
+import React, {useState} from 'react'
+import {FcGoogle} from 'react-icons/fc'
+import {FaGithub} from 'react-icons/fa'
+import { SignInFlow } from '../types/types'
+
+
+
+interface SignUpCardProps {
+
+  setState: (state: SignInFlow) => void
+}
+
+export const SignUpCard = ({setState}: SignUpCardProps) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+
+  return (
+    <Card className='w-full h-full p-12'>
+      <img src={'/flash.png'} alt='Logo' className='w-12 h-12 mb-0 mx-auto'/>
+      <CardHeader className='px-0 pt-8'>
+        <CardTitle>Sign Up to Continue</CardTitle>
+        <CardDescription>
+        Use your email or another service to continue
+      </CardDescription>
+      </CardHeader>
+    
+      <CardContent className='space-y-5 px-0 pb-0'>
+        {/* Sign in form elements will go here */}
+        <form className='space-y-2.5'>
+          <Input
+          disabled={false}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Email'
+          type='email'
+          required
+          />
+
+          <Input
+          disabled={false}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='Password'
+          type='password'
+          required
+          />
+
+          <Input
+          disabled={false}
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          placeholder='Confirm Password'
+          type='password'
+          required
+          />
+
+          <Button type='submit'className='w-full bg-black/70 hover:bg-black cursor-pointer' size="lg" disabled={false}>Continue</Button>
+
+          
+        </form>
+        <Separator/>
+
+        <div className='flex flex-col gap-y-2.5'>
+          <Button disabled={false}
+          onClick={() => {}}
+          variant={'outline'}
+          size={'lg'}
+          className='w-full relative flex items-center justify-center gap-x-2.5 cursor-pointer'
+          >
+            <FcGoogle size={20} />
+            Continue with Google
+          </Button>
+
+          <Button disabled={false}
+          onClick={() => {}}
+          variant={'outline'}
+          size={'lg'}
+          className='w-full relative flex items-center justify-center gap-x-2.5  cursor-pointer'
+          >
+            <FaGithub size={20} />
+            Continue with Github
+          </Button>
+        </div>
+        <p>
+         Already have an account? <span onClick={()=> setState("signIn")} className='text-sky-700 hover:underline cursor-pointer'>Sign in</span>
+        </p>
+
+      </CardContent>
+    </Card>
+  )
+}
--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -2,12 +2,21 @@
 
 import { useState } from "react"
 import { SignInFlow } from "../types/types"
+import { SignInCard } from "./SignInCard"
+import { SignUpCard } from "./SignUpCard"
 
 export const AuthScreen = () => {
     const [state, setState] = useState<SignInFlow>("signIn")
     return (
-        <div className="h-full flex items-center justify-center">
-            Auth Screen
+        <div className="relative h-screen flex items-center justify-center bg-[#36393f] overflow-hidden">
+            
+            {/* Decorative gradient shapes */}
+            <div className="absolute -top-32 -left-32 w-125 h-125 bg-linear-to-br from-[#7289da] to-[#99aab5] rounded-full opacity-30 blur-3xl"></div>
+            <div className="absolute -bottom-40 -right-40 w-150 h-150 bg-linear-to-br from-[#99aab5] to-[#7289da] rounded-full opacity-20 blur-3xl"></div>
+
+            <div className="relative md:h-auto md:w-105">
+                {state === "signIn" ? <SignInCard setState={setState} /> : <SignUpCard setState={setState} />}
+            </div>
         </div>
     )
 }
