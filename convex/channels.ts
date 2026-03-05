14efe02d89593021d3be2c128dc073d076a3fcda
Syed Laeeq Ahmed <laeeqe@icloud.com>
2026-03-06 03:40:38 +0500

    feat(channel): implement update and delete channel mutations with frontend API hooks

--- convex/channels.ts
+++ convex/channels.ts
@@ -98,8 +98,75 @@ export const getById = query({
 export const update = mutation({
     args: {
         id: v.id("channels"),
-        workspaceId: v.id("workspaces"),
         name: v.string()
     },
-    handler: async (ctx, args) => {}
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx);
+
+        if(!userId){
+            throw new Error("Unauthorized");
+        }
+
+        const channel = await ctx.db.get(args.id);
+
+        if(!channel){
+            throw new Error("Channel not found");
+        }
+
+        const member = await ctx.db
+        .query("members")
+        .withIndex("byWorkspaceId_user_id", (q) =>
+        q.eq("workspaceId", channel.workspaceId).eq("userId", userId)).unique()
+
+        if(!member || member.role !== "admin"){
+            throw new Error("Unauthorized");
+        }
+
+    
+
+        await ctx.db.patch(args.id, {
+            name: args.name
+        })
+
+        return args.id
+    }
+
+})
+
+
+
+export const remove = mutation({
+    args: {
+        id: v.id("channels"),
+      
+    },
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx);
+
+        if(!userId){
+            throw new Error("Unauthorized");
+        }
+
+        const channel = await ctx.db.get(args.id);
+
+        if(!channel){
+            throw new Error("Channel not found");
+        }
+
+        const member = await ctx.db
+        .query("members")
+        .withIndex("byWorkspaceId_user_id", (q) =>
+        q.eq("workspaceId", channel.workspaceId).eq("userId", userId)).unique()
+
+        if(!member || member.role !== "admin"){
+            throw new Error("Unauthorized");
+        }
+
+    
+
+        await ctx.db.delete(args.id)
+
+        return args.id
+    }
+
 })
