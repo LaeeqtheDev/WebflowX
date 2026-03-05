3c85ffa736e21577dd14944f72718d1276a0e191
Syed Laeeq Ahmed <laeeqe@icloud.com>
2026-03-05 06:23:33 +0500

    feat(workspace): implement workspace header with dynamic workspace data

--- src/app/dashboard/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/dashboard/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,7 +1,41 @@
+"use client"
+
+import { useGetChannel } from "@/features/channels/api/use-get-channel";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { Loader, TriangleAlert, TriangleAlertIcon } from "lucide-react";
+import { Header } from "../../components/header";
+
 const channelIdPage = () => {
+    const channelId = useChannelId();
+    const {data: channel, isLoading: channelLoading} = useGetChannel({id: channelId})
+
+    if(channelLoading) 
     return(
-        <div>
-            Hi
+        <div className="h-full flex-1 flex items-center justify-center">
+            <Loader className="animate-spin size-5 text-[#ff5018]"/>
+
+        </div>
+
+        ) 
+
+
+        if( !channel) 
+        return(
+            <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+                <TriangleAlert className="size-5 text-[#ff5018]"/>
+                <span className="text-sm text-muted-foreground">
+                    Channel not found
+                </span>
+            </div>
+    
+            ) 
+    
+
+
+    return(
+        <div className="flex flex-col h-full">
+            <Header title={channel.name}/>
+         
         </div>
     )
 }
