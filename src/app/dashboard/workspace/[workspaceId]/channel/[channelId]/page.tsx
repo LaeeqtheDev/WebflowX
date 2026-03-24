be6646581f9058ef3602265ea56b9b7367ce2235
Syed Laeeq Ahmed <laeeqe@icloud.com>
2026-03-24 15:58:30 +0500

    feat(messages): implement MessageList component with frontend integration

--- src/app/dashboard/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/dashboard/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -6,14 +6,15 @@ import { Loader, TriangleAlert, TriangleAlertIcon } from "lucide-react";
 import { Header } from "../../components/header";
 import { ChatInput } from "../../components/Chat-Input";
 import { useGetMessages } from "@/features/messages/api/use-get-messages";
+import { MessageList } from "../../components/message-list";
 
 const channelIdPage = () => {
     const channelId = useChannelId();
 
-    const {results} = useGetMessages({channelId})
+    const {results, status, loadMore} = useGetMessages({channelId})
     const {data: channel, isLoading: channelLoading} = useGetChannel({id: channelId})
 
-    if(channelLoading) 
+    if(channelLoading || status === "LoadingFirstPage") 
     return(
         <div className="h-full flex-1 flex items-center justify-center">
             <Loader className="animate-spin size-5 text-[#ff5018]"/>
@@ -37,11 +38,17 @@ const channelIdPage = () => {
 
 
     return(
+        
         <div className="flex flex-col h-full">
             <Header title={channel.name}/>
-            <div className="flex-1">
-                {JSON.stringify(results)}
-            </div>
+            <MessageList
+            channelName={channel.name}
+            channelCreationTime={channel._creationTime}
+            data={results}
+            loadMore={loadMore}
+            isLoadingMore={status === "LoadingMore"}
+            canLoadMore={status === "CanLoadMore"}
+            />
             <ChatInput placeholder={`Message #${channel.name}`} />
 
            
